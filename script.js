03910402d5586914c4fcdf397f7387dd2ed61862
nathmunhoz <nmunhoz80@gmail.com>
2025-11-10 15:14:03 -0300

    added some logic flow to the js

---
+++ script.js
@@ -0,0 +1,29 @@
+// gameboard (as an array) -> inside -> gameboard object
+// players stored in objects
+// object o control the flow of the game itself
+
+// game, player, gameboard -> objects
+
+// start as a console game
+
+// all inside factories
+
+/*  
+    GAME -- 
+    1. chhecking when the game is over (3 rounds)
+    2. checking for all winning 3-in-a-rows AND ties
+*/
+
+// OBJECT that will handle the display/DOM logic
+
+/*  
+    GAMEBOARD -- 
+    1. function that allow players to add marks to a 
+    specific spot on the board by interacting with the 
+    DOM elements (clicking on a board square)
+    2. DO NOT let players play in spots that are already taken
+    3. Clean up the interface to allow players to put in their names, 
+    include a button to start/restart the game and add a display element 
+    that shows the results upon game end!
+    4. function that will render the contents of the gameboard array to the webpage
+*/
